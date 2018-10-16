4e4a372c73f2dbc2dbe0e4cc1cfd5d88982eb12d
Moses Lee <leecmoses@gmail.com>
2018-10-16 15:39:25 -0400

    Completed Lesson 1

---
+++ 01-welcome-to-the-program/notes.py
@@ -0,0 +1,12 @@
+###############
+## Lesson 01 ##
+###############
+
+'''
+1. Welcome to the program!
+2. Meet your instructors
+3. What you'll build
+4. Support and community
+5. Calendar and deadlines
+    * To keep up with this schedule, we find that most students have to invest about 10 hours a week (or just under 1.5 hours per day) of focused study time.
+'''
