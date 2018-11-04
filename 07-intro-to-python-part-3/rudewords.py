fc1b386eaabd886990998675ed6bd52e07bf98f2
Moses Lee <leecmoses@gmail.com>
2018-11-04 15:13:55 -0500

    Completed Working with Files portion of Lesson 7

---
+++ 07-intro-to-python-part-3/rudewords.py
@@ -0,0 +1,33 @@
+# Rude words detector, version 0.2
+
+import string
+
+
+rude_words = ["crap", "darn", "heck", "jerk", "idiot", "butt", "devil"]
+
+
+def check_line(line):
+    rude_count = 0
+    words = line.split(" ")
+    for word in words:
+        word = word.strip(string.punctuation).lower()
+        print(f"Words: {word}")
+        if word in rude_words:
+            rude_count += 1
+            print(f"Found rude word: {word}")
+    return rude_count
+
+
+def check_file(filename):
+    with open(filename) as myfile:
+        rude_count = 0
+        for line in myfile:
+            rude_count += check_line(line)
+
+    if rude_count == 0:
+        print("Congratulations, your file has no rude words.")
+        print("At least, no rude words I know.")
+
+
+if __name__ == '__main___':
+    check_file("my_story.txt")
