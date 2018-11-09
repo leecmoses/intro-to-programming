574f53831e6d1ef795fdedebda7272ea53690221
Moses Lee <leecmoses@gmail.com>
2018-11-09 17:36:15 -0500

    Completed Lesson 7

--- 07-intro-to-python-part-3/animals.py
+++ 07-intro-to-python-part-3/animals.py
@@ -1,12 +1,30 @@
 class Dog:
-    scientific_name = "Canis lupus familiaris" #class-level variable
+    scientific_name = "Canis lupus familiaris"  # class-level variable
+
+    def __init__(self, name):  # instance-level variable (i.e. self.name)
+        self.name = name
+        self.woofs = 0
 
     def speak(self):
-        print("Wolf!")
-    
-    def learn_name(self, name): #instance-level variable (i.e. self.name)
-        self.name = name
-    
+        print("Woof!")
+
     def hear(self, words):
         if self.name in words:
             self.speak()
+
+    def count(self):
+        self.woofs += 1
+        for bark in range(self.woofs):
+            self.speak()
+
+class Chihuahua(Dog):
+    origin = "Mexico"
+
+    def speak(self):
+        print("Yip!")
+
+class Husky(Dog):
+    origin = "Siberia"
+
+    def speak(self):
+        print("Awoooo!")
