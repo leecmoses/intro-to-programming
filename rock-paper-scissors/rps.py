edc3e0194594b48eea600d02413c6bb6034d7dec
Moses Lee <leecmoses@gmail.com>
2018-11-12 11:28:37 -0500

    Add color to display

--- rock-paper-scissors/rps.py
+++ rock-paper-scissors/rps.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import random
+from colorama import Fore, Style
 """This program plays a game of Rock, Paper, Scissors between two Players,
 and reports both Player's scores each round."""
 
@@ -116,7 +117,8 @@ class Game:
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
-        print(f"You played {move1}.\nOpponent played {move2}.")
+        print(Fore.RED + f"You played {move1}.")
+        print(Fore.BLUE + f"Opponent played {move2}." + Style.RESET_ALL)
         if move1 == move2:
             print("The round is a tie!")
         elif beats(move1, move2):
@@ -131,14 +133,17 @@ class Game:
 
     def play_game(self):
         for round in range(1, select_rounds() + 1):
-            print(f"\n-- Round {round} --")
+            print(Fore.YELLOW + f"\n-- Round {round} --" + Style.RESET_ALL)
             self.play_round()
         if self.p1.score > self.p2.score:
-            print(f"\nYou won the game with {self.p1.score} point(s)!")
+            print(Fore.YELLOW +
+                  f"\nYou won the game with {self.p1.score} point(s)!")
         elif self.p2.score > self.p1.score:
-            print(f"\nOpponent won the game with {self.p2.score} point(s)!")
+            print(Fore.YELLOW +
+                  f"\nOpponent won the game with {self.p2.score} point(s)!")
         else:
-            print(f"\nYou and the opponent tied! Play again!")
+            print(Fore.YELLOW +
+                  f"\nYou and the opponent tied! Play again!")
 
 
 if __name__ == '__main__':
